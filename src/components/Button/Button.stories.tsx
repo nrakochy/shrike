55f5e6ca6d3d43e3e2d8789e9d19e864b0c9a86f
Nick <nick.rakochy@gmail.com>
2020-08-20 00:21:54 -0500

    Leverage theme in component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
-
-import { Button, ButtonProps as ThemeButtonProps } from "theme-ui";
-
-interface ButtonProps extends ThemeButtonProps {
-  label: string;
-}
+import { Button, ButtonProps } from "./Button";
 
 export default {
   title: "Button",
   component: Button,
   argTypes: {
     label: { control: "text" },
-    mr: { control: { type: "number", min: 0, max: 10 } },
   },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => {
-  const { label, ...rest } = args;
-  return <Button {...rest}>{label}</Button>;
+  return <Button {...args} />;
 };
 
 export const Primary = Template.bind({});
 Primary.args = {
   label: "Button",
+  bg: "secondary",
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,48 +1,13 @@
-import React from 'react';
-import './button.css';
+import React from "react";
+import {
+  Button as ThemeButton,
+  ButtonProps as ThemeButtonProps,
+} from "theme-ui";
 
-export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
+export interface ButtonProps extends ThemeButtonProps {
   label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button: React.FC<ButtonProps> = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
+  return <ThemeButton {...props}>{label}</ThemeButton>;
 };
